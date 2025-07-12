66a67172eb0a9f078464fbe42534a9e9d3a2c49f
Damian <Damian@DESKTOP-272OFED>
2025-07-12 14:35:00 +0200

    Fixed bugs

--- server/app.js
+++ server/app.js
@@ -2,20 +2,19 @@ import dotenv from "dotenv";
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
-
-import ConnectionManager from "./managers/ConnectionManager";
-import ClientRoutes from "./routes/ClientRoutes";
+import ConnectionManager from "./managers/ConnectionManager.js";
+// import ClientRoutes from "./routes/ClientRoutes";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const clientRoutes = new ClientRoutes();
+// const clientRoutes = new ClientRoutes();
 
 dotenv.config();
 
 // Tutaj ustawiamy jakie pliki strony internetowej będzie wysyłał nasz serwer
 app.use(express.static("clients"));
-app.use("/", clientRoutes.getRouter());
+// app.use("/", clientRoutes.getRouter());
 
 // Tutaj ustawiamy w na jakie event w komunikacji socket-io będziemy nasłuchiwać i reagować
 io.on("connection", (socket) => {
