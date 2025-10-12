48404f8e245737e36434273e6f78324006a6733f
Damian Sikora <me@imienazwisko.com>
2025-10-12 19:17:01 +0200

    expanded disconnect logic

---
+++ server/errors/GameAbortedPlayerLeftError.js
@@ -0,0 +1,7 @@
+export default class GameAbortedPlayerLeftError extends Error {
+    constructor(message = "Gra zakończona z powodu rozłączenia gracza.") {
+        super(message);
+        this.name = "GameAbortedPlayerLeftError";
+        this.code = "GAME_ABORTED_PLAYER_LEFT_ERROR";
+    }
+}
