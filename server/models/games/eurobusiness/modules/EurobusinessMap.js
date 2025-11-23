fc5250952faa03173eeea14606863bb61b6d6677
Dawid Chawrona <76160943+chawrona@users.noreply.github.com>
2025-11-24 00:03:14 +0100

    Update EurobusinessMap to use fields property
    Refactor EurobusinessMap to use fields from mapData.

--- server/models/games/eurobusiness/modules/EurobusinessMap.js
+++ server/models/games/eurobusiness/modules/EurobusinessMap.js
@@ -2,16 +2,18 @@ import mapData from "../config/gameMap.json" with { type: "json" };
 
 export default class EurobusinessMap {
     constructor() {
-        this.map = mapData;
+        this.fields = mapData;
     }
 
     movePlayer(player, number) {
-        let position = player.getData("position");
+        const position = player.getData("position");
         let newPosition = position + number;
+        
         if (newPosition > 39) {
             newPosition -= 40;
             return true;
         }
+        
         return false;
     }
 }
