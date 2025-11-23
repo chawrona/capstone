19ac0caa37f0a5c4fd5c72416f93c356bed4154d
Dawid B <bulinet2401@gmail.com>
2025-11-23 17:38:47 +0100

    Create basic walking system for Eurobusiness

--- server/models/games/eurobusiness/modules/EurobusinessMap.js
+++ server/models/games/eurobusiness/modules/EurobusinessMap.js
@@ -1,1 +1,17 @@
-export default class EurobusinessMap {}
+import mapData from "../config/gameMap.json" with { type: "json" };
+
+export default class EurobusinessMap {
+    constructor() {
+        this.map = mapData;
+    }
+
+    movePlayer(player, number) {
+        let position = player.getData("position");
+        let newPosition = position + number;
+        if (newPosition > 39) {
+            newPosition -= 40;
+            return true;
+        }
+        return false;
+    }
+}
