959b6bac16c0654f0c2b9b586bb83da8f96ce23a
chawrona <dawidchawrona@gmail.com>
2025-11-25 14:48:28 +0100

    conflict

--- server/models/games/eurobusiness/interfaces/tileTypes.js
+++ server/models/games/eurobusiness/interfaces/tileTypes.js
@@ -1,10 +1,7 @@
 const tileTypes = {
     default: "Domyślne",
     start: "Start",
-<<<<<<< HEAD
-=======
     parking: "Parking",
->>>>>>> 3499159fee0f664f3e25dd15ce7edd274a16bbd3
 };
 
 export default tileTypes;
