04e57e0bc521e83eaae61557aaf8f65aa2a465c2
chawrona <dawidchawrona@gmail.com>
2026-02-06 07:38:30 +0100

    ASDAD

--- server/models/games/brianboru/BrianBoru.js
+++ server/models/games/brianboru/BrianBoru.js
@@ -1,6 +1,7 @@
 import getRandomNumber from "../../../utils/getRandomNumber.js";
 import Game from "../../Game.js";
-import cards from "./cards.js";
+import cards from "./config/cards.js";
+import statuses from "./config/status.js";
 
 export default class BrianBoru extends Game {
     constructor(players, endGame, lobbyId) {
@@ -25,6 +26,8 @@ export default class BrianBoru extends Game {
         this.gameData.passedCardsPlayerCount = 0;
         this.gameData.passedCardsData = [];
 
+        // Status graczy - co robią
+
         // Śluby - pola od 1 do 9, bez zerowego
         // zapamiętać, ZAWSZE jak się ruszać to pytasz o dokupienie
         // potem nie ma możliwości, jak masz za mało to spadasz niżej
@@ -38,6 +41,9 @@ export default class BrianBoru extends Game {
         player.setData("vikings", () => 0);
         player.setData("suns", () => 1);
         player.setData("money", () => 5);
+        player.setData("lockedCards", () => new Set());
+        player.setData("cards", () => []);
+        this.setPlayerStatus(player, statuses.REJECT_CARDS);
     }
 
     start() {
@@ -61,19 +67,86 @@ export default class BrianBoru extends Game {
         }
     }
 
+    resetCardDrawPhase() {
+        for (const player of this.players.values()) {
+            this.setPlayerStatus(player, statuses.REJECT_CARDS);
+            player.setData("cards", () => []);
+            player.setData("lockedCards", () => new Set());
+        }
+
+        this.initalizeRandomCards();
+        this.giveCardsToPlayers();
+        this.gameData.passedCardsPlayerCount = 0;
+        this.gameData.passedCardsData = [];
+        this.gameData.phases.passing.current += 1;
+    }
+
     // @event
-    selectCardsToPass() {
-        // id1, id2, będą w data.data
-        // zabierz graczowi 2 karty które wybrał
-        // wrzuć je do tablicy [karta, karta, indeksGracza]
-        // dodaj tablicę do danych
-        // jak był to ostatni gracz to uruchom funkcję
-        this.passCards();
+    selectCardsToPass(data) {
+        const player = this.getPlayer(data.publicId);
+        const cardsToPass = data.data;
+
+        const lockedCards = player.getData("lockedCards");
+        for (const card of cardsToPass) {
+            if (lockedCards.includes(card)) {
+                throw new Error(
+                    `Nie możesz przekazać karty, którą dostałeś: ${card}`,
+                );
+            }
+        }
+
+        player.setData("cards", (oldCards) => {
+            const newCards = oldCards.filter((card) => {
+                for (const cardToPass of cardsToPass) {
+                    if (cardToPass === card.id) return false;
+                }
+                return true;
+            });
+
+            return newCards;
+        });
+
+        this.gameData.passedCardsData.push = [data.publicId, ...cardsToPass];
+
+        this.gameData.passedCardsPlayerCount++;
+        if (this.gameData.passedCardsPlayerCount === this.players.size) {
+            return this.passCards();
+        } else {
+            this.setPlayerStatus(player, statuses.REJECT_CARDS_WAITING);
+            return this.generateGameData(data.publicId);
+        }
     }
 
     passCards() {
-        // Weź dane z passedCardsData i przekaż je graczom
-        // zmień fazę wybierania na następną lub na granie
+        for (const cardsToPass of this.gameData.passedCardsData) {
+            const [publicId, ...lockedCards] = cardsToPass;
+            const playerIndex = this.playersQueue.indexOf(publicId);
+            const nextPlayerIndex =
+                (playerIndex + 1) % this.playersQueue.length;
+
+            const nextPlayer = this.getPlayer(
+                this.playersQueue[nextPlayerIndex],
+            );
+
+            nextPlayer.setData("lockedCard", (oldSet) => {
+                lockedCards.forEach((card) => oldSet.add(card));
+                return oldSet;
+            });
+        }
+
+        this.gameData.passedCardsPlayerCount = 0;
+
+        const passPhase = (this.gameData.phases.passing.current += 1);
+        if (passPhase === this.gameData.phases.passing.total) {
+            // Tutaj będziemy udawać, że cała faza gry przeszła i tylko resetować na ponowne wybieranie kart.
+
+            return this.sendGameDataToAll();
+        } else {
+            for (const player of this.players.values()) {
+                this.setPlayerStatus(player, statuses.REJECT_CARDS);
+            }
+            return this.sendGameDataToAll();
+        }
     }
 
     // Odsyłanie danych
@@ -107,32 +180,57 @@ export default class BrianBoru extends Game {
     }
 
     generateGameData(publicId) {
-        return {
-            // Karty gracza po lewej
-            cards: this.getPlayer(publicId).getData("cards"),
+        return [
+            {
+                target: publicId,
+                eventName: "gameData",
+                data: {
+                    // Karty gracza po lewej
+                    cards: [
+                        this.getPlayer(publicId).getData("cards"),
+                        this.getPlayer(publicId).getData("lockedCards"),
+                    ],
 
-            // Śluby na dole po lewej
-            marriages: this.gameData.marriages.map((object) => object.color),
+                    // Śluby na dole po lewej
+                    marriages: this.gameData.marriages.map(
+                        (object) => object.color,
+                    ),
 
-            // Kościół na dole po prawej
-            church: this.generateChurchData(),
+                    // Kościół na dole po prawej
+                    church: this.generateChurchData(),
 
-            // Gracze po prawej u góry
-            players: this.generatePlayerData(),
+                    // Gracze po prawej u góry
+                    players: this.generatePlayerData(),
 
-            // Wikingowie u góry po lewej
-            currentVikings: this.gameData.currentVikings,
-        };
+                    // Wikingowie u góry po lewej
+                    currentVikings: this.gameData.currentVikings,
+
+                    status: this.getPlayerStatus(publicId),
+                    phases: this.gameData.phases,
+                },
+            },
+        ];
+    }
+
+    sendGameDataToAll() {
+        const data = [];
+        for (const publicId of this.players.keys()) {
+            data.push(this.generateGameData(publicId)[0]);
+        }
+        return data;
     }
 
     gameDataRequest(data) {
-        return [
-            {
-                target: data.publicId,
-                eventName: "gameData",
-                data: this.generateGameData(data.publicId),
-            },
-        ];
+        return this.generateGameData(data.publicId);
+    }
+
+    // Pomocnicze
+    getPlayerStatus(publicId) {
+        this.getPlayer(publicId).getData("status");
+    }
+
+    setPlayerStatus(player, status) {
+        player.setData("status", () => status);
     }
 }
 
@@ -151,3 +249,7 @@ np. jeżeli faza wybieranie kart do odrzucenia to odsyłamy event z pokazaniem j
 Trzeba jakoś zaznaczyć których kart nie możesz odrzucić, bo już je odrzuciłeś.
 
 */
+
+// DODATKOWY EVENT NA POINFORMOWANIE DIALOGIEM JAKIE KARTY OTRZYMAŁEŚ
+
+// Animacja gdy wejdzie nowa faza wybierania kart, tak żeby ukryć że zmieniają się nagle bez dialogu
---
+++ server/models/games/brianboru/config/status.js
@@ -0,0 +1,6 @@
+const statuses = {
+    REJECT_CARDS: 0,
+    REJECT_CARDS_WAITING: 0,
+};
+
+export default statuses;
