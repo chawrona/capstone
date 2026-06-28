8a9bbc0d908f5675427f90fa0e2d6a886f76ca02
chawrona <dawidchawrona@gmail.com>
2026-06-28 13:10:28 +0200

    Removed Ludo and Eurobusiness games

--- server/models/games/eurobusiness/interfaces/communityCardTypes.js
+++
@@ -1,9 +0,0 @@
-const communityCardTypes = {
-    withdrawCashFromBank: "Pobierz pieniądze z banku",
-    payCashToBank: "Zapłać bankowi",
-    goToJail: "Idź do więzienia",
-    getOutOfJail: "Wyjdź z więzenia",
-    payTaxes: "Zapłać bankowi podatki",
-};
-
-export default communityCardTypes;
